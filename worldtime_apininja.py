8c775a01777a69c95fd9bfec0ff71b72c4781cbb
antonlovg <karlanton.lovgren@gmail.com>
2023-09-21 18:11:04 +0200

    Åtgärdat val 3 - lookup favorites

--- worldtime_apininja.py
+++ worldtime_apininja.py
@@ -78,8 +78,8 @@ def city_information(city):
 def print_city_info(city_info):
     ui.line()
     ui.echo(f"The city {city_info['city']} is in the timezone of {city_info['timezone']}.")
-    ui.echo(f"Today it is {city_info['day_of_week']} and the date is {city_info['date']} and the time is {city_info['time']}.")
-    ui.line()
+    ui.echo(
+        f"Today it is {city_info['day_of_week']} and the date is {city_info['date']} and the time is {city_info['time']}.")
 
 
 # Val 1
@@ -90,6 +90,7 @@ def new_city():
     # Utskrift
     print_city_info(city_info)
 
+    ui.line()
     # Låter användaren spara sin favorit
     favorit = ui.prompt("Do you want to save this city as your favorite? (j/n)").lower()
 
@@ -102,6 +103,7 @@ def new_city():
         ui.prompt(f"You chose to save {city_to_check} as a favorite, press enter to continue")
 
     elif favorit == "n":
+
         ui.prompt(f"You chose to not save {city_to_check} as a favorite, press enter to continue")
 
     else:
@@ -118,8 +120,7 @@ def compare_cities():
     city1_info = city_information(city1)
     city2_info = city_information(city2)
 
-
-    # För att ta fram endast positivt tal
+    # För att ta fram endast positivt tal används abs
     # https://www.w3schools.com/python/ref_func_abs.asp
     time_difference = abs(int(city1_info['hour']) - int(city2_info['hour']))
 
@@ -128,6 +129,31 @@ def compare_cities():
     ui.echo(f"The time difference between {city1} and {city2} is {time_difference} hours")
     ui.line()
     ui.prompt("Press enter to continue")
+
+
+# Val 3
+def lookup_favorites():
+    if not favorites:
+        ui.prompt("You dont have any favorite cities saved, press enter to continue")
+
+    else:
+        for city in favorites:
+            city_info = city_information(city)
+            print_city_info(city_info)
+        ui.line()
+        ui.prompt("Press enter to continue")
+
+        # Loopa igenom allt
+        # Lägg in ny stad varje loop
+        # Använd print_city_info för utskrift
+
+# Val 4
+#def delete_favorites():
+    #ui.echo("Your favorites right now")
+    #for n in favorites:
+    #    ui.echo(n)
+    #delete = ui.prompt("What city do you want to remove")
+    #favorites.pop()
 
 
 # -- PROGRAMMET -- #
@@ -147,13 +173,16 @@ while True:
 
     ui.line()
     ui.header("Choose an option")
+    ui.line()
     ui.echo("1 - Check a new city")
     ui.echo("2 - Compare cities")
     ui.echo("3 - Lookup your favorite/s")
     ui.echo("4 - Delete a favorite")
+    ui.line()
 
     # Användaren får ett val
     choice = ui.prompt("Type your choice")
+    ui.line()
 
     # Kollar upp en ny stad
     if choice == "1":
@@ -161,7 +190,7 @@ while True:
     elif choice == "2":
         compare_cities()
     elif choice == "3":
-        print("")
+        lookup_favorites()
     elif choice == "4":
         print("")
     else:
