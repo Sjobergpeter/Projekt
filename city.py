ebc4bbe50e7eae78cc9a818ea0f1516a7176c58f
Johan Siegers <j-siegers@outlook.com>
2023-09-25 22:38:55 +0200

    Ny meny & city.json aktiverad

--- city.py
+++ city.py
@@ -96,17 +96,27 @@ class City:
 
 # Skapar objektet som innehåller variabler om efterfrågad stad
 city_obj = City()
-counter = 1  # Räknare för om inmatning av stad ska startas
+
 # Om en stad är sparad i city.json så läses den in
 try:
     city_obj.input_city = city[0]
-    # Ritar up UI för första körningen
-    ui.clear()
-    ui.line()
-    ui.header("CITY DATA")
-    ui.line()
+
 except IndexError:
-    counter = 0
+    print('Error, the city is not in the database')
+# Ritar up UI för första körningen
+City.city_information(city_obj)
+holiday_info = City.get_holiday_api(city_obj)
+ui.clear()
+ui.line()
+ui.header("CITY DATA")
+ui.line()
+# Resultatet från sökningen skrivs ut
+print(f'\n{city_obj.city_name} is a city in {city_obj.country}\nand {city_obj.capital}.')
+print(f'The population of {city_obj.city_name} is {city_obj.population:,}.')
+print(f'The location of the city is at \nlatitude {city_obj.latitude} and longitude\n{city_obj.longitude}.')
+ui.line()
+input('> Press Enter to continue')
+ui.clear()
 
 while True:
     # UI
@@ -114,45 +124,63 @@ while True:
     ui.line()
     ui.header("CITY DATA")
     ui.line()
-    # Om ingen stad finns sparad från huvudmenyn, skriv in en manuellt
-    if counter < 1:
-        # Startar gränsnittet och inmatning av stad
-        City.city_start()
-    counter = 0  # Räknaren nollställs så att manuell inmatning av stad sker nästa gång loopen körs
-
-    # Hämtar data från API:erna city och holiday
-    City.city_information(city_obj)
-    holiday_info = City.get_holiday_api(city_obj)
-    holiday_list = []
-
-    # Resultatet från sökningen skrivs ut
-    print(f'\n{city_obj.city_name} is a city in {city_obj.country}\nand {city_obj.capital}.')
-    print(f'The population of {city_obj.city_name} is {city_obj.population:,}.')
-    print(f'The location of the city is at \nlatitude {city_obj.latitude} and longitude {city_obj.longitude}.')
-
+    # todo: Show favourites
     ui.line()
-    print(f'Press 1 to show major holidays in {city_obj.country}')
-    print('Press 2 to search for another city')
-    print('Press 3 to return to main menu')
+
+    ui.header("Choose an option")
+    ui.line()
+    print('1 | Search for a city')
+    print('2 | Lookup your favourite/s')
+    print('3 | Delete a favourite')
+    print('4 | return to main menu')
+    ui.line()
     menu = input(':> ')
+    # Startar menyfunktionen
 
     if menu == '1':
-        print(f'\n| Major holidays 2023 in {city_obj.country} |\n')
+        # Sökmetoden anropas
+        City.city_start()
+        # Hämtar data från API:erna city och holiday
+        City.city_information(city_obj)
+        holiday_info = City.get_holiday_api(city_obj)
+        holiday_list = []
 
-        # Loop som sparar dictionaryt i en lista efter datum och namn
-        for i in holiday_info:
-            holiday_list.append([i['date'], i['name']])
+        # Resultatet från sökningen skrivs ut
+        print(f'\n{city_obj.city_name} is a city in {city_obj.country}\nand {city_obj.capital}.')
+        print(f'The population of {city_obj.city_name}\nis {city_obj.population:,}.')
+        print(f'The location of the city is at \nlatitude {city_obj.latitude} and\nlongitude {city_obj.longitude}.')
+        ui.line()
+        holidays = ui.prompt(f'Would you like to see major\nholidays in {city_obj.country} (yes/no)?').lower()
+        ui.line()
+        if holidays == 'yes':
+            print(f'\n| Major holidays 2023 in {city_obj.country} |\n')
 
-        # Helgdagslistan sorteras i datumordning och skrivs sedan ut
-        holiday_list.sort(key=lambda item: item[0])
-        for holiday in holiday_list:
-            print(holiday[0], holiday[1])
+            # Loop som sparar dictionaryt i en lista efter datum och namn
+            for i in holiday_info:
+                holiday_list.append([i['date'], i['name']])
+
+            # Helgdagslistan sorteras i datumordning och skrivs sedan ut
+            holiday_list.sort(key=lambda item: item[0])
+            for holiday in holiday_list:
+                print(holiday[0], holiday[1])
+            ui.line()
+            ui.prompt('Press Enter to continue')
+
+        else:
+            ui.clear()
+            continue
 
     elif menu == '2':
         ui.line()
         ui.clear()
         continue
+
     elif menu == '3':
+        ui.line()
+        ui.clear()
+        continue
+
+    elif menu == '4':
         break
 
 
