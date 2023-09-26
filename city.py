b9f27daf93e5d722624d867c4d83b6f4b60997f1
Johan Siegers <j-siegers@outlook.com>
2023-09-26 11:51:23 +0200

    Ny meny & favorit aktiverad

--- city.py
+++ city.py
@@ -27,13 +27,6 @@ class City:
         self.longitude = longitude
         self.input_city = input_city
 
-    # Metod som startar UI och inmatning
-    @staticmethod
-    def city_start():
-        ui.line()
-        city_obj.input_city = input('Type a city name: > ')
-        return city_obj.input_city
-
     # Metod för att hämta city information från API
     @staticmethod
     def get_city_api(self):
@@ -45,6 +38,7 @@ class City:
             return response_dict
         else:
             print("Error:", response.status_code, response.text)
+            return
 
     # Metod för att hämta holiday information från API
     @staticmethod
@@ -53,7 +47,15 @@ class City:
         try:
             country = city_dict[0]["country"]
         except IndexError:
-            return 'No country found'
+            print('No country found')
+            return
+        except ValueError:
+            print('ValueError exception occured')
+            return
+        except Exception as e:
+            # Hanterar alla andra undantag
+            print("An unknown exception occured:", e)
+            return
         year = '2023'
         holiday_name = 'major_holiday'
         api_url = 'https://api.api-ninjas.com/v1/holidays?country={}&year={}&type={}'.format(country, year,
@@ -72,7 +74,12 @@ class City:
         try:
             self.city_name = city_dict[0]["name"]
         except IndexError:
-            return 'The city does not exist in the database'
+            print('The city does not exist in the database')
+            return
+        except Exception as e:
+            # Hanterar alla andra undantag
+            print("An unknown exception occured:", e)
+            return
 
         # Omvandlar capital true/false till text
         if city_dict[0]["is_capital"]:
@@ -93,53 +100,22 @@ class City:
         self.longitude = city_dict[0]["longitude"]
         return self
 
+    @staticmethod
+    def your_favorite():
+        ui.echo("Your favorite places:")
 
-# Skapar objektet som innehåller variabler om efterfrågad stad
-city_obj = City()
+        if not favorites:
+            ui.echo("No favorite is saved right now")
 
-# Om en stad är sparad i city.json så läses den in
-try:
-    city_obj.input_city = city[0]
+        for i in favorites:
+            ui.echo(i)
 
-except IndexError:
-    print('Error, the city is not in the database')
-# Ritar up UI för första körningen
-City.city_information(city_obj)
-holiday_info = City.get_holiday_api(city_obj)
-ui.clear()
-ui.line()
-ui.header("CITY DATA")
-ui.line()
-# Resultatet från sökningen skrivs ut
-print(f'\n{city_obj.city_name} is a city in {city_obj.country}\nand {city_obj.capital}.')
-print(f'The population of {city_obj.city_name} is {city_obj.population:,}.')
-print(f'The location of the city is at \nlatitude {city_obj.latitude} and longitude\n{city_obj.longitude}.')
-ui.line()
-input('> Press Enter to continue')
-ui.clear()
+        ui.line()
 
-while True:
-    # UI
-    ui.clear()
-    ui.line()
-    ui.header("CITY DATA")
-    ui.line()
-    # todo: Show favourites
-    ui.line()
-
-    ui.header("Choose an option")
-    ui.line()
-    print('1 | Search for a city')
-    print('2 | Lookup your favourite/s')
-    print('3 | Delete a favourite')
-    print('4 | return to main menu')
-    ui.line()
-    menu = input(':> ')
-    # Startar menyfunktionen
-
-    if menu == '1':
-        # Sökmetoden anropas
-        City.city_start()
+    # Metod som startar sökning av stad, utskrift av info & helgdagar & spara favorit
+    @staticmethod
+    def city_start():
+        city_obj.input_city = input('Type a city name: > ')
         # Hämtar data från API:erna city och holiday
         City.city_information(city_obj)
         holiday_info = City.get_holiday_api(city_obj)
@@ -147,11 +123,12 @@ while True:
 
         # Resultatet från sökningen skrivs ut
         print(f'\n{city_obj.city_name} is a city in {city_obj.country}\nand {city_obj.capital}.')
-        print(f'The population of {city_obj.city_name}\nis {city_obj.population:,}.')
+        print(f'The population of {city_obj.city_name} \nis {city_obj.population:,}.')
         print(f'The location of the city is at \nlatitude {city_obj.latitude} and\nlongitude {city_obj.longitude}.')
         ui.line()
         holidays = ui.prompt(f'Would you like to see major\nholidays in {city_obj.country} (yes/no)?').lower()
         ui.line()
+        # Om användaren vill visa holidays körs denna kod
         if holidays == 'yes':
             print(f'\n| Major holidays 2023 in {city_obj.country} |\n')
 
@@ -163,17 +140,85 @@ while True:
             holiday_list.sort(key=lambda item: item[0])
             for holiday in holiday_list:
                 print(holiday[0], holiday[1])
-            ui.line()
-            ui.prompt('Press Enter to continue')
+
+        ui.line()
+        # Låter användaren spara sin favorit
+        favorit = ui.prompt("Do you want to save this city as your favorite? (y/n)").lower()
+
+        if favorit == "y":
+            favorites.append(city_obj.city_name)
+
+            with open("favorites.json", "w+") as b:
+                json.dump(favorites, b)
+
+            ui.prompt(f"You chose to save {city_obj.city_name} as a favorite, press enter to continue")
+
+        elif favorit == "n":
+
+            ui.prompt(f"You chose not to save {city_obj.city_name} as a favorite, press enter to continue")
 
         else:
-            ui.clear()
-            continue
+            input("ERROR!")
+        # return city_obj.input_city
 
+
+# Skapar objektet som innehåller variabler om efterfrågad stad
+city_obj = City()
+
+# Om en stad är sparad i city.json så läses den in
+try:
+    city_obj.input_city = city[0]
+
+except IndexError:
+    print('Error, the city is not in the database')
+
+# Ritar up UI för första körningen
+City.city_information(city_obj)
+ui.clear()
+ui.line()
+ui.header("CITY DATA")
+ui.line()
+# Resultatet från sökningen skrivs ut
+print(f'\n{city_obj.city_name} is a city in {city_obj.country}\nand {city_obj.capital}.')
+print(f'The population of {city_obj.city_name} \nis {city_obj.population:,}.')
+print(f'The location of the city is at \nlatitude {city_obj.latitude} and longitude\n{city_obj.longitude}.')
+ui.line()
+input('> Press Enter to continue')
+ui.clear()
+
+# Huvudprogrammet med menyfunktion
+while True:
+    # UI
+    ui.clear()
+    ui.line()
+    ui.header("CITY DATA")
+    ui.line()
+    ui.echo("Your favorite places:")
+    if not favorites:
+        ui.echo("No favorite is saved right now")
+
+    for favorite in favorites:
+        ui.echo(favorite)
+
+    ui.line()
+    ui.line()
+    ui.header("Choose an option")
+    ui.line()
+    print('1 | Search for a city')
+    print('2 | Lookup your favorite/s')
+    print('3 | Delete a favorite')
+    print('4 | return to main menu')
+    ui.line()
+    menu = input(':> ')
+
+    # Startar menyfunktionen
+    if menu == '1':
+        # Sökmetoden anropas
+        City.city_start()
+
+    # Listar favoritstäder
     elif menu == '2':
-        ui.line()
-        ui.clear()
-        continue
+        City.your_favorite()
 
     elif menu == '3':
         ui.line()
@@ -182,6 +227,3 @@ while True:
 
     elif menu == '4':
         break
-
-
-
