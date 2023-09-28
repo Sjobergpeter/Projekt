4c5dd91aa63d26c55ea683992a99af911e217b31
Anton Lövgren <karlanton.lovgren@gmail.com>
2023-09-28 11:44:36 +0200

    Rensat filer

--- huvudkod.py
+++ huvudkod.py
@@ -1,27 +1,32 @@
 import ui
 import json
 import worldtime
+# Lägg till weather och gruppuppgift
+# import weather
+# import gruppuppgift
+# import city
 
 city_choice = ""
 
 while True:
     ui.clear()
-
+    ui.line()
+    ui.header("")
     # Väljer stad samt lägger in den i city.json
-    city = ui.prompt("Välj en stad ")
+    choose_city = ui.prompt("Pick a city")
     with open("city.json", "w+") as f:
-        f.write(json.dumps(city))
+        f.write(json.dumps(choose_city))
 
     # Global variabel i funktionen från worldtime
-    worldtime.get_city_choice(city)
+    worldtime.get_city_choice(choose_city)
 
     ui.line()
 
-    val = ui.prompt("Välj en siffra ")
+    val = ui.prompt("Pick a choice")
 
     ui.line()
     if val == "1":
-        worldtime.main(city)
+        worldtime.main(choose_city)
     elif val == "2":
         import gruppuppgift
     elif val == "3":
